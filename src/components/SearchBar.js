88063c4258bbf10e064b4b67b803ade8c7759777
Leeyanhawrt <leeyan1108@gmail.com>
2023-07-18 00:33:15 -0400

    Conclude pics app

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,13 @@
+import './SearchBar.css';
 import { useState } from 'react'
 
 function SearchBar({ onSubmit }) {
-  const [term, setTerm] = ('');
+  const [term, setTerm] = useState('');
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
 
-    onSubmit('cars')
+    onSubmit(term)
   };
 
   const handleChange = (event) => {
@@ -14,8 +15,9 @@ function SearchBar({ onSubmit }) {
   };
 
   return (
-    <div>
+    <div className="search-bar">
       <form onSubmit={handleFormSubmit}>
+        <label>Enter Search Term</label>
         <input value={term} onChange={handleChange} />
       </form>
     </div>
